f92d07b03c1395c2c2d5302226d15b74f0e3e2af
Rachel Margulies <rachelmrgls@gmail.com>
2015-05-04 12:11:04 -0400

    fixed

--- js/student.js
+++ js/student.js
@@ -1,17 +1,8 @@
 var Student = {
     // please fill in your name and NetID
     // your NetID is the part of your email before @princeton.edu
-<<<<<<< Updated upstream
-=======
-<<<<<<< HEAD
     'name'  : 'The Winners',
     'netID' : 'pinner',
-=======
->>>>>>> Stashed changes
-    'name'  : 'The Winnerz',
-    'netID' : 'winner',
->>>>>>> origin/master
-};
 
 Student.updateHTML = function( ) {
     var studentInfo = this.name + ' &lt;' + this.netID + '&gt;';
